13d530c46546be5f0ae0f7d6e28349f7045e6e99
ChavesVolodymyr <151963528+ChavesVolodymyr@users.noreply.github.com>
2025-03-02 21:53:43 +0100

    111

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,8 +3,8 @@ const formData = {
   message: '',
 };
 
-const form = document.querySelector('.form-feedback');
-const localStorageKey = 'form-feedback-state';
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
 
 function saveFormData() {
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
@@ -15,10 +15,8 @@ function fillUpForm() {
 
   if (savedData) {
     const savedFormData = JSON.parse(savedData);
-    formData.email = savedFormData.email;
-    formData.message = savedFormData.message;
-    form.elements.email.value = formData.email;
-    form.elements.message.value = formData.message;
+    formData.email.elements.value = savedFormData.email;
+    formData.message.elements.value = savedFormData.message;
   }
 }
 
@@ -29,20 +27,25 @@ function clearFormData() {
 fillUpForm();
 
 form.addEventListener('input', () => {
-  formData.email = form.elements.email.value;
-  formData.message = form.elements.message.value.trim();
-  saveFormData();
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value.trim(),
+  };
+  saveFormData(formData);
 });
 
 form.addEventListener('submit', event => {
   event.preventDefault();
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value.trim(),
+  };
+
   if (formData.email === '' || formData.message === '') {
     alert('Fill please all fields');
   } else {
-    console.log('Form', formData);
-    clearFormData();
-    formData.email = '';
-    formData.message = '';
+    console.log('Form:', formData);
+    localStorage.removeItem(localStorageKey);
     form.reset();
   }
 });
